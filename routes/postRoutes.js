99e9168d04308baa2d2273c700c8831b3f4cd2e6
Ofek Segman <ofeksegman@gmail.com>
2024-11-22 15:42:30 +0200

    afterfix

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,10 +5,6 @@ const postController = require('../controller/postController');
 router.post('/',postController.createPost);
 router.get('/', postController.getAllPosts);
 router.get('/:id', postController.getPostById);
-<<<<<<< HEAD
-=======
-//router.get('/:senderId', postController.getPostBySenderId);
->>>>>>> ac2374ce2783f592b49316f4a90b9ab0e613da7e
 router.put('/:id', postController.updatePost);
 
 module.exports = router;
