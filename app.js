36e9ead1306d7e54c80ccc66f61231048200343d
Ofek Segman <ofeksegman@gmail.com>
2024-11-22 12:51:41 +0200

    Check

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 const dotenv = require('dotenv').config();
 const port=process.env.PORT;
 require('dotenv/config');
-bodyParser = require('body-parser');
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DB_CONNECT);
 const db = mongoose.connection;
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const  postmodel = require('../model/post');
-
 const postController = require('../controller/postController');
-router.post('/', postController.createPost);
-router.get('/', postController.getPosts);
-router.get('/:id', postController.getPostById);
+
+router.post('/',postController.createPost);
+//router.get('/', postController.getPosts);
+//router.get('/:id', postController.getPostById);
 
 module.exports = router;
