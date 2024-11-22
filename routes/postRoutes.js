ac2374ce2783f592b49316f4a90b9ab0e613da7e
NatiEzra <128730088+NatiEzra@users.noreply.github.com>
2024-11-22 15:01:31 +0200

    .

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,7 +5,7 @@ const postController = require('../controller/postController');
 router.post('/',postController.createPost);
 router.get('/', postController.getAllPosts);
 router.get('/:id', postController.getPostById);
-router.get('/:senderId', postController.getPostBySenderId);
+//router.get('/:senderId', postController.getPostBySenderId);
 router.put('/:id', postController.updatePost);
 
 module.exports = router;
