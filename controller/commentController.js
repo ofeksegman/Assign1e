8b464575e8f93e408d4ada0ce220cdf2bb21998e
NatiEzra <128730088+NatiEzra@users.noreply.github.com>
2024-11-24 19:09:18 +0200

    read update and delete comments

--- controller/commentController.js
+++ controller/commentController.js
@@ -8,7 +8,47 @@ const createComment = async (req, res) => {
         res.status(400).send(error.message);
     }
 }
+
+const getAllComments= async (req, res) => {
+    try {
+        const comments = await commentModel.find();
+        res.status(200).send(comments);
+        } catch (error) {
+        res.status(400).send(error.message);
+        }
+    };
+
+const updateComment= async (req, res) => {
+    const {id} = req.params;
+    const commentBody = req.body;
+    try {
+        const comment = await commentModel.findByIdAndUpdate(id,   commentBody, { new: true });
+        
+        if(!comment) {
+            res.status(404).send('comment not found');
+        }
+        return res.status(200).json(comment);
+    }
+    catch (error) {
+        res.status(400).send(error.message);
+    }
+};
+
+const deleteComment= async (req, res) => { 
+    const { id } = req.params;
+    try {
+        const comment = await commentModel.findByIdAndDelete(id);
+        if (!comment) {
+            res.status(404).send('comment not found');
+        }
+        return res.status(200).json(comment);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+};
 module.exports = { 
     createComment,
-
+    getAllComments,
+    updateComment,
+    deleteComment
  };
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const commentController = require('../controller/commentController');
 
 router.post('/',commentController.createComment);
-
+router.get('/', commentController.getAllComments);
+router.put('/:id', commentController.updateComment);
+router.delete('/:id', commentController.deleteComment);
 
 module.exports = router;
